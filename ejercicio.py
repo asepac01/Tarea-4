2628cdd875cea70b8adf15fdd25f20f50fad409f
Lorely Sepa <lorelyangie01@gmail.com>
2021-06-16 13:15:49 -0500

    Ejercicio 2 completado.

--- ejercicio.py
+++ ejercicio.py
@@ -11,7 +11,11 @@ class For:
         lista_notas = [(30, 40), [20, 40] ,(50, 40)]
         lista_estudiantes = [{"nombre": "Erick", "final": 70}, {"nombre": "Yady", "final": 60}, {"nombre": "Danny", "final": 90}]
 
-        for i in range(5):
+        # for i in range(5):
+        #     print("i: {}".format(i))
+        
+        print("")
+        for i in range(2, 10):
             print("i: {}".format(i))
 
 
